c6de203e69441388556f8fe3dbd604c4aef6c521
Mugisha Jean Baptiste <johnbutista004@gmail.com>
2026-01-05 13:09:11 +0200

    feat: work on all method as it required

--- BuildBudgetApp.py
+++ BuildBudgetApp.py
@@ -1,5 +1,54 @@
 class Category:
-    pass
+    def __init__(self, name):
+        self.name = name
+        self.ledger = []
+
+    def deposit(self, amount, description=""):
+        self.ledger.append({
+            "amount": amount,
+            "description": description
+        })
+
+    def withdraw(self, amount, description=""):
+        if self.check_funds(amount):
+            self.ledger.append({
+                "amount": -amount,
+                "description": description
+            })
+            return True
+        return False
+
+    def get_balance(self):
+        total = 0
+        for item in self.ledger:
+            total += item["amount"]
+        return total
+
+    def transfer(self, amount, category):
+        if self.check_funds(amount):
+            self.withdraw(amount, f"Transfer to {category.name}")
+            category.deposit(amount, f"Transfer from {self.name}")
+            return True
+        return False
+
+    def check_funds(self, amount):
+        return amount <= self.get_balance()
+    def __str__(self):
+        title = self.name.center(30,'*') + '\n'
+        items =''
+        total = 0
+        for item in self.ledger:
+             items += f"{item['description'][:23]:23}{item['amount']:7.2f}\n"
+             total += item['amount']
+        return title + items + f"Total: {total:.2f}"
 
 def create_spend_chart(categories):
     pass
+
+# food = Category('Food')
+# food.deposit(1000, 'deposit')
+# food.withdraw(10.15, 'groceries')
+# food.withdraw(15.89, 'restaurant and more food for dessert')
+# clothing = Category('Clothing')
+# food.transfer(50, clothing)
+# print(food)
