32a234af5566becc94109eaa1e04c95c3b435546
Mugisha Jean Baptiste <johnbutista004@gmail.com>
2026-01-21 19:51:06 +0200

    feat: inheritane basic

---
+++ inheritance_basic.py
@@ -0,0 +1,14 @@
+
+class Animal:
+    def __init__(self, name):
+        self.name = name
+
+    def sound(self):
+        return f'{self.name} makes a sound'
+
+class Dog(Animal):
+    bark = 'woof! woof!! woof!!!'
+
+jack = Dog('Jack')
+print(jack.sound())  # Jack makes a sound
+print(jack.bark)  # woof! woof!! woof!!!
