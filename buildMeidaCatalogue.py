649a33d3920777d266d3a6899ee6b51771c09544
Mugisha Jean Baptiste <johnbutista004@gmail.com>
2026-01-26 14:58:15 +0200

    feat: working on the polymorphisim and build medical catalogue

---
+++ buildMeidaCatalogue.py
@@ -0,0 +1,17 @@
+class Movie:
+    def __init__(self, title, year, director, duration):
+        if not title.strip():
+            raise ValueError('Title cannot be empty')
+        if year < 1895:
+            raise ValueError('Year must be 1895 or later')
+        if(not director or not director.strip()):
+            raise ValueError('Director cannot be empty')
+        self.title = title
+        self.year = year
+        self.director = director
+        self.duration = duration
+    def __str__(self):
+        return f'{self.title} ({self.year}) - {self.duration} min, {self.director}'
+
+movie1 = Movie('The Matrix', 1999, 'The Wachowskis', 136)
+print(movie1)
