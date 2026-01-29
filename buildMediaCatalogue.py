2615fef90022e981faaa1f8f2f676a8cadf31f96
Mugisha Jean Baptiste <johnbutista004@gmail.com>
2026-01-29 09:02:33 +0200

    feat: make function to call the movies and series

--- buildMediaCatalogue.py
+++ buildMediaCatalogue.py
@@ -66,9 +66,15 @@ class MediaCatalogue:
         if not self.items:
             return 'Media Catalogue (empty)'
 
+        movies = self.get_movies()
+        series = self.get_tv_series()
+
         result = f'Media Catalogue ({len(self.items)} items):\n\n'
-        for i, movie in enumerate(self.items, 1):
-            result += f'{i}. {movie}\n'
+        if movies:
+            result += '=== MOVIES ===\n'
+            for i, movie in enumerate(movies, 1):
+                result += f'{i}. {movie}\n'
+
         return result
 
 
