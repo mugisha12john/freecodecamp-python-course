6ccce94ccd354c9b0abbdd010d65df8ead145c8a
Mugisha Jean Baptiste <johnbutista004@gmail.com>
2026-01-29 14:30:09 +0200

    feat: adding abstract class

--- buidDiscountCalculatorAbastraction.py
+++ buidDiscountCalculatorAbastraction.py
@@ -1,3 +1,5 @@
+from abc import ABC, abstractmethod
+
 class Product:
     def __init__(self, name: str, price: float) -> None:
         self.name = name
@@ -5,6 +7,11 @@ class Product:
 
     def __str__(self) -> str:
         return f'{self.name} - ${self.price}'
+
+class DiscountStrategy(ABC):
+    @abstractmethod
+    def is_applicable(self,product: Product,user_tier: str):
+        pass
 
 product = Product('Wireless Mouse', 50.0)
 print(product)
