1ee72aff688bd781df62351a1d26039316449771
Mugisha Jean Baptiste <johnbutista004@gmail.com>
2026-02-04 21:32:22 +0200

    feat: build a polygon area calculator using inheritance

---
+++ buildPolygonAreaCalculator.py
@@ -0,0 +1,53 @@
+import math
+class Rectangle:
+    def __init__(self,width,height):
+        self.width = width
+        self.height = height
+
+    def set_width(self,w):
+        self.width = w
+    def set_height(self,h):
+        self.height = h
+    def get_area(self):
+        return self.width*self.height
+    def get_perimeter(self):
+        return 2*(self.width * self.height)
+    def get_diagonal(self):
+        return math.sqrt((self.width**2 + self.height**2))
+    def get_picture(self):
+        if self.width > 50 or self.height > 50:
+            return 'Too big for picture'
+        result = ''
+        for x in range(self.height):
+            result += '*' * self.width
+            result += '\n'
+        return result
+    def get_amount_inside(self,square):
+        pass
+class Square(Rectangle):
+    def __init__(self,side,width,height):
+        super().__init__(width,height)
+        self.side =side
+    def set_width(self,w):
+        self.width = w
+    def set_height(self,h):
+        self.height = h
+    def set_side(self):
+        self.side = self.width + self.height
+rect = Rectangle(10, 5)
+print(rect.get_area())
+rect.set_height(3)
+print(rect.get_perimeter())
+print(rect)
+print(rect.get_picture())
+
+sq = Square(9)
+print(sq.get_area())
+sq.set_side(4)
+print(sq.get_diagonal())
+print(sq)
+print(sq.get_picture())
+
+rect.set_height(8)
+rect.set_width(16)
+# # print(rect.get_amount_inside(sq))
