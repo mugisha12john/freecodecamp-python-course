7aa2284dbf7939ef2c54f26ba2d8b26192fbcbe7
Mugisha Jean Baptiste <johnbutista004@gmail.com>
2026-02-18 17:08:03 +0200

    feat: build hash table

---
+++ buildHashTable.py
@@ -0,0 +1,5 @@
+# You should define a class named HashTable with a collection attribute initialized to an empty
+# dictionary when a new instance of HashTable is created. The collection dictionary should store key-value pairs based
+# on the hashed value of the key.
+#
+# The HashTable class should have four instance methods: hash, add, remove, and lookup.
