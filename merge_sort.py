6d34525795dd7622c7d96af624e287f79fc3c396
Mugisha Jean Baptiste <johnbutista004@gmail.com>
2026-02-27 20:18:30 +0200

    feat: work on it

---
+++ merge_sort.py
@@ -0,0 +1,27 @@
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    mid = len(arr) // 2
+    left = merge_sort(arr[:mid])
+    right = merge_sort(arr[mid:])
+
+    sorted_list = []
+    i = 0
+    j = 0
+
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            sorted_list.append(left[i])
+            i += 1
+        else:
+            sorted_list.append(right[j])
+            j += 1
+
+    sorted_list.extend(left[i:])
+    sorted_list.extend(right[j:])
+
+    return sorted_list
+
+# print(merge_sort([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]))
+print(merge_sort([42,37,53,17]))
