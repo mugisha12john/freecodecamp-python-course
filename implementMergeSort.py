650befb7118e283a4420736a9a08f9ec1bba3582
Mugisha Jean Baptiste <johnbutista004@gmail.com>
2026-03-10 15:26:11 +0200

    feat: final codes for implementMergeSort

--- implementMergeSort.py
+++ implementMergeSort.py
@@ -32,3 +32,9 @@ def merge_sort(array):
         right_array_index += 1
         sorted_index += 1
 
+
+if __name__ == '__main__':
+    numbers = [4, 10, 6, 14, 2, 1, 8, 5]
+    print('Unsorted array: ')
+    print(numbers)
+    merge_sort(numbers)
